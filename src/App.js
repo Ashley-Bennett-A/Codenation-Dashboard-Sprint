b131e6e42ce6032dd6d89ace3faa6b65a706bf98
Connor Rooney <connorrooney.ap@gmail.com>
2019-07-10 12:35:59 +0100

    weather basic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import News from "./Components/News";
 import "./App.css";
-import DateTime from "./Components/DateTime.jsx";
+import DateTime from "./Components/DateTime";
 import Search from "./Components/Search";
+import Weather from './Components/Weather'
 
 function App() {
   return (
     <div className="App">
       <DateTime />
+      <Weather />
       <News />
       <Search />
     </div>
