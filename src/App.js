8ca6e578f7c417732e2611d4ab01c5c5f4486144
Connor Rooney <connorrooney.ap@gmail.com>
2019-07-10 14:49:28 +0100

    svgs

--- src/App.js
+++ src/App.js
@@ -8,15 +8,14 @@ import UsefulLinks from "./Components/UsefulLinks";
 import Games from "./Components/Games";
 import Chat from "./Components/Chat";
 import ChuckNorris from "./Components/ChuckNorris";
+import Header from "./Components/Header";
 
 function App() {
   return (
     <div className="App">
-      <center>
-        <h1 className="titleIndex"> about: blank </h1>
-      </center>
+      <Header />
       <DateTime />
-      <Weather />
+      {/* <Weather /> */}
       <UsefulLinks />
       <News />
       <Search />
