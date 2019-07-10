6c852c0744b6871d6680cde1a11bc8e89b6a7d31
Ashley Benntt <ashleyben21@gmail.com>
2019-07-10 14:12:14 +0100

    merged

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import News from "./Components/News";
 import "./App.css";
 import DateTime from "./Components/DateTime";
 import Search from "./Components/Search";
-import Weather from './Components/Weather'
+import Weather from "./Components/Weather";
 import UsefulLinks from "./Components/UsefulLinks";
 import Games from "./Components/Games";
 import Chat from "./Components/Chat";
@@ -12,15 +12,16 @@ import ChuckNorris from "./Components/ChuckNorris";
 function App() {
   return (
     <div className="App">
-      <center><h1 className="titleIndex">about:blank</h1></center>
+      <center>
+        <h1 className="titleIndex"> about: blank </h1>
+      </center>
       <DateTime />
       <Weather />
       <UsefulLinks />
       <News />
       <Search />
       <Chat />
-      <ChuckNorris />
-      {/* <Games /> */}
+      <ChuckNorris /> {/* <Games /> */}{" "}
     </div>
   );
 }
