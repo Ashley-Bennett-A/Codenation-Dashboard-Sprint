2bbe3114f98aa39b5fc1c7a9bf31e5686ecd31be
Ashley Benntt <ashleyben21@gmail.com>
2019-07-16 11:58:42 +0100

    Updates

---
+++ precache-manifest.1bdd6b91ca2a387e12106647baa8755e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4510abfd6efc67e696c444aa90f1642b",
+    "url": "/Codenation-Dashboard-Sprint/index.html"
+  },
+  {
+    "revision": "0468cdcca7d05042debf",
+    "url": "/Codenation-Dashboard-Sprint/static/css/main.56b830db.chunk.css"
+  },
+  {
+    "revision": "8d804993b7533ccdb2e8",
+    "url": "/Codenation-Dashboard-Sprint/static/js/2.4f5674ce.chunk.js"
+  },
+  {
+    "revision": "0468cdcca7d05042debf",
+    "url": "/Codenation-Dashboard-Sprint/static/js/main.f191154e.chunk.js"
+  },
+  {
+    "revision": "ddba0ac7a18fb9e145c3",
+    "url": "/Codenation-Dashboard-Sprint/static/js/runtime~main.161c45f4.js"
+  },
+  {
+    "revision": "edf55b9f78e6f53751c3c3a30c9d5e96",
+    "url": "/Codenation-Dashboard-Sprint/static/media/bada55_io.edf55b9f.png"
+  },
+  {
+    "revision": "3bb4ef9ada87413d9d60c751ad753c26",
+    "url": "/Codenation-Dashboard-Sprint/static/media/chuck_norris.3bb4ef9a.png"
+  },
+  {
+    "revision": "3fc5b5134e2be742c3b6cc9fa4ff8d45",
+    "url": "/Codenation-Dashboard-Sprint/static/media/codepen.3fc5b513.png"
+  },
+  {
+    "revision": "040e76c047b64f77a84372686c570a33",
+    "url": "/Codenation-Dashboard-Sprint/static/media/codesandbox.040e76c0.svg"
+  },
+  {
+    "revision": "c469bf91245ba288adfa7ce3a627ef4a",
+    "url": "/Codenation-Dashboard-Sprint/static/media/csstricks.c469bf91.png"
+  },
+  {
+    "revision": "84580f1293875510b7e3581a74f1af57",
+    "url": "/Codenation-Dashboard-Sprint/static/media/fontawesome.84580f12.svg"
+  },
+  {
+    "revision": "b517b3a130f955002f73dd2149f17f39",
+    "url": "/Codenation-Dashboard-Sprint/static/media/googlefonts.b517b3a1.png"
+  },
+  {
+    "revision": "7fb94052ec4988596239d41687d2abaa",
+    "url": "/Codenation-Dashboard-Sprint/static/media/hexacolor.7fb94052.png"
+  },
+  {
+    "revision": "800ccca91c7fce141bbe1adadb3b4919",
+    "url": "/Codenation-Dashboard-Sprint/static/media/shape1.800ccca9.svg"
+  },
+  {
+    "revision": "1d67f8af511fa864825045ffc275b556",
+    "url": "/Codenation-Dashboard-Sprint/static/media/shape2.1d67f8af.svg"
+  },
+  {
+    "revision": "ffd2d566901306fa9c228305995c9005",
+    "url": "/Codenation-Dashboard-Sprint/static/media/shape3.ffd2d566.svg"
+  },
+  {
+    "revision": "f74b43957a47464dddcef810819fb7fb",
+    "url": "/Codenation-Dashboard-Sprint/static/media/shape4.f74b4395.svg"
+  },
+  {
+    "revision": "92d70decc80627caab900582f519b6aa",
+    "url": "/Codenation-Dashboard-Sprint/static/media/stackOverflow.92d70dec.svg"
+  }
+]);
